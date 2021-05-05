471ef4a24ad1a6180c2f41e3075a6aff20006238
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-05-05 16:01:24 -0700

    add datejs and create a week view component

--- app/src/App.js
+++ app/src/App.js
@@ -1,95 +1,48 @@
 import * as React from "react";
 
-import * as apiClient from "./apiClient";
+// import * as apiClient from "./apiClient";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Day from "./components/Day";
+import Week from "./components/Week";
 import logo from "./moon-logo.svg";
 
 const App = () => {
-  const [tasks, setTasks] = React.useState([]);
-
-  const today = new Date();
-  const [date, setDate] = React.useState(today);
-  const loadTasks = async () => setTasks(await apiClient.getTasks());
-
-  React.useEffect(() => {
-    loadTasks();
-  }, []);
-
-  const formatDate = () => {
-    const daysOfWeek = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    let day = daysOfWeek[date.getDay()];
-    let dd = String(date.getDate()).padStart(2, "0");
-    let mm = String(date.getMonth() + 1).padStart(2, "0");
-    let yyyy = date.getFullYear();
-
-    let dateValue = day + " " + mm + "/" + dd + "/" + yyyy;
-    return dateValue;
-  };
-
-  const changeDateValue = ({ addValue }) => {
-    setDate(date.setDate(date.getDate() + addValue));
-  };
-
   return (
-    <main className="App">
-      <nav>
-        <img id="header-logo" src={logo} alt="Lunar Task Logo" />
-        <h1>Lunar Task</h1>
-      </nav>
-      <section>
-        <h2>{formatDate()}</h2>
-        <AddTask loadTasks={loadTasks} />
-        <TaskList tasks={tasks} />
-      </section>
-      <section>
-        <Day />
-      </section>
-    </main>
-  );
-};
-
-const TaskList = ({ tasks }) => (
-  <ul>
-    {tasks.map(({ id, name }) => (
-      <li key={id}>
-        {name}
-        <button>Edit</button>
-        <button>Delete</button>
-      </li>
-    ))}
-  </ul>
-);
-
-const AddTask = ({ loadTasks }) => {
-  const [task, setTask] = React.useState("");
-
-  const canAdd = task !== "";
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    if (canAdd) {
-      await apiClient.addTask(task);
-      loadTasks();
-      setTask("");
-    }
-  };
-
-  return (
-    <form onSubmit={onSubmit}>
-      <label>
-        New task:{" "}
-        <input onChange={(e) => setTask(e.currentTarget.value)} value={task} />
-      </label>
-      <button disabled={!canAdd}>Add</button>
-    </form>
+    <Router>
+      <main className="App">
+        <nav>
+          <img id="header-logo" src={logo} alt="Lunar Task Logo" />
+          <h1>Lunar Task</h1>
+          <ul>
+            <li key={1}>
+              <Link to="/">Day View</Link>
+            </li>
+            <li key={2}>
+              <Link to="/week">Week View</Link>
+            </li>
+            <li key={3}>
+              <Link to="/calendar">Calendar View</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/">
+            <section>
+              <Day />
+            </section>
+          </Route>
+          <Route path="/week">
+            <section>
+              <Week />
+            </section>
+          </Route>
+          <Route path="/calendar">
+            <h1>Calendar view</h1>
+          </Route>
+        </Switch>
+      </main>
+    </Router>
   );
 };
 
