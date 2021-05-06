7e6d69f022a7bf7459dfd54a69dc305a84280b77
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-05-06 09:23:53 -0700

    add example code

--- server/db.mjs
+++ server/db.mjs
@@ -4,6 +4,7 @@ import pgp from "pg-promise";
 const db = initDb();
 
 export const getTasks = async () => await db.any("SELECT * FROM tasks");
+//week get dates[0] dates[6] and query the range
 
 export const addTask = async (name) =>
   (
